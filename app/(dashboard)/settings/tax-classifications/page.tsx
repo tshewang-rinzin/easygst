59220e42b8559d716a7171d5de4e0fe26f20eb94
Tshewang Rinzin <trigdzeen@gmail.com>
2026-03-20 01:56:29 +0600

    ui: fix spacing on settings/tax-classifications page

--- app/(dashboard)/settings/tax-classifications/page.tsx
+++ app/(dashboard)/settings/tax-classifications/page.tsx
@@ -385,7 +385,7 @@ export default function TaxClassificationsPage() {
 
   return (
     <section className="flex-1 p-4 lg:p-8">
-      <div className="max-w-4xl">
+      <div>
         <div className="mb-6">
           <Link
             href="/settings"
